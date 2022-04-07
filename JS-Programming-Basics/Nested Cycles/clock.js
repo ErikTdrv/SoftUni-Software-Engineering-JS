aa1c1bf5efa84f87e7932dcfc4e21017c25d6eee
Erik Todorov <eriktdrv@gmail.com>
2022-04-07 10:11:33 +0300

    Adding clock solved problem

---
+++ JS-Programming-Basics/Nested Cycles/clock.js
@@ -0,0 +1,20 @@
+function clock (){
+
+
+
+
+    for (let i = 0; i <= 23; i++){
+        for (let m = 0; m <= 59; m++){
+            console.log(`${i}:${m}`)
+        }
+    }
+
+
+
+
+
+
+
+
+}
+clock()
