4c8a0f9839a0b2b4f6ea50f27c3b71c417745fc3
Erik Todorov <eriktdrv@gmail.com>
2022-04-08 09:06:29 +0300

    Adding AnimalType solved problem

---
+++ JS-Programming-Basics/Harder Checks/animalType.js
@@ -0,0 +1,16 @@
+function animalType(input){
+
+    let animalName = input[0];
+
+    switch(animalName){
+        case 'dog': console.log('mammal');break;
+        case 'crocodile' :
+        case 'tortoise' : 
+        case 'snake' : console.log('reptile');break;
+        default : console.log('unknown');
+
+    }
+
+
+}
+animalType(['cat'])
