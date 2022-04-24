f0740df38a95b592c03e696322b5185654d8fa77
Erik Todorov <eriktdrv@gmail.com>
2022-04-24 10:22:47 +0300

    Adding NxNMatrix solved problem

---
+++ JS-Fundamentals/First Part/Functions/Exercises/07. NxN Matrix.js
@@ -0,0 +1,14 @@
+function nxnMatrix(a){
+// Write a function that receives a single integer number n and prints nxn matrix with that number.
+    let result = []
+
+    for (let i = a; i > 0; i--){
+        for (let j = 0; j < a; j++){
+            result += (a + ' ') 
+            
+        }
+        console.log(result)
+        result = ''
+    }
+}
+nxnMatrix(7)
