df8d701a31bc1982ee737f2befc8557e8a56b043
Erik Todorov <eriktdrv@gmail.com>
2022-05-02 21:28:37 +0300

    Adding Variables solved problem

---
+++ JS-Fundamentals/First Part/Variables/requiredReading.js
@@ -0,0 +1,18 @@
+function requiredReading(pageNumbers, pagesRead1H, daysForReading){
+
+let totalHoursNeeded = pageNumbers/pagesRead1H
+let requiredHoursPerDay = totalHoursNeeded /daysForReading
+console.log(requiredHoursPerDay)
+
+
+
+
+
+
+
+
+
+
+
+}
+requiredReading(212,20 ,2)
