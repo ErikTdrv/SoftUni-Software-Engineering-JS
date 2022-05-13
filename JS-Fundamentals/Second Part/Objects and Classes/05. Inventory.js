78cc65c442e2a6881cbd38f17b4e6995e1750a8f
Erik Todorov <eriktdrv@gmail.com>
2022-05-13 20:10:55 +0300

    Adding Inventory solved problem

---
+++ JS-Fundamentals/Second Part/Objects and Classes/05. Inventory.js
@@ -0,0 +1,30 @@
+function inventory(arr){
+
+    let heros = [];
+    for (let el of arr){
+        let info = el.split(' / ')
+        let hero = info[0]
+        let level = info[1]
+        let items = info[2].split(', ').join(', ')
+        let currentHero = {
+            hero,
+            level,
+            items
+        }
+        heros.push(currentHero)
+        
+    }
+    heros.sort((a,b) => a.level - b.level)
+    for (let hero of heros){
+        console.log(`Hero: ${hero.hero}`)
+        console.log(`level => ${hero.level}`)
+        console.log(`items => ${hero.items}`)
+    }
+
+
+}
+inventory([
+    'Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara'
+    ])
