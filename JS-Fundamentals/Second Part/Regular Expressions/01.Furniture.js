3cbdf50ed1c97b0275e8e52eff55d12648e4b8c0
Erik Todorov <eriktdrv@gmail.com>
2022-05-14 21:58:48 +0300

    Adding Furniture solved problem

---
+++ JS-Fundamentals/Second Part/Regular Expressions/01.Furniture.js
@@ -0,0 +1,30 @@
+function furniture(input){
+
+    console.log('Bought furniture:')
+    let totalSum = 0;
+    
+    
+    for (let line of input){
+        if (line === 'Purchase'){
+            break;
+        }
+        let regExp = />>(?<name>[A-Za-z\s]+)<<(?<price>\d+(.\d+)?)!(?<quantity>\d+)/
+        let result = regExp.exec(line)
+        if (result !== null){
+            console.log(result.groups.name)
+            totalSum += +result.groups.price * +result.groups.quantity
+        }
+        
+    }
+    console.log(`Total money spend: ${totalSum.toFixed(2)}`)
+}
+furniture(['>>Laptop<<312.2323!3',
+'>>TV<<300.21314!5',
+'>Invalid<<!5',
+'>>TV<<300.21314!20',
+'>>Invalid<!5',
+'>>TV<<30.21314!5',
+'>>Invalid<<!!5',
+'Purchase']
+
+)
