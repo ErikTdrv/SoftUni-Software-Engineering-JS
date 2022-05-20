c4a137be075616aff0c9b1a9a687e2c920d816e9
Erik Todorov <eriktdrv@gmail.com>
2022-05-20 11:35:02 +0300

    Fixing PreviousDay solved problem

--- JS-Advanced/01. Syntax, Functions and Statements/04. PreviousDay.js
+++ JS-Advanced/01. Syntax, Functions and Statements/04. PreviousDay.js
@@ -1,19 +1,11 @@
 function previousDay(year, month, day){
 
 
-    const today = new Date(year, month, day)
+    const date = new Date(year, month - 1, day)
     
-    const tomorrow = new Date(today)
-    tomorrow.setDate(tomorrow.getDate() - 1)
-    
-    if (year %4 === 0){
-        let day = Number(tomorrow.getDate()) - 1
-        console.log(tomorrow.getFullYear() + '-' + tomorrow.getMonth() + '-' + day)
-    }else{
-        console.log(tomorrow.getFullYear() + '-' + tomorrow.getMonth() + '-' + tomorrow.getDate())
+    date.setDate(date.getDate() - 1)
+    console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`)
 
-    }
-    
-    
+
 }
 previousDay(2016, 10, 1)
