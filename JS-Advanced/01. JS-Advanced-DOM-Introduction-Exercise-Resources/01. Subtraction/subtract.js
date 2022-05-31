83c69e20433201aebc3ed8db887681187949849c
Erik Todorov <eriktdrv@gmail.com>
2022-05-31 20:09:31 +0300

    Editing folder

--- JS-Advanced/01. JS-Advanced-DOM-Introduction-Exercise-Resources/01. Subtraction/subtract.js
+++
@@ -1,8 +0,0 @@
-function subtract() {
-    let firstElement = document.getElementById('firstNumber').value;
-    let secondElement = document.getElementById('secondNumber').value;
-    let divElement = document.getElementById('result')
-    let result = Number(firstElement) - Number(secondElement);
-    divElement.textContent = result;
-
-}
