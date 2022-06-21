9a2bdb2561cea331b100579fedecb57c1ddfb689
Erik Todorov <eriktdrv@gmail.com>
2022-06-21 17:18:46 +0300

    Adding Rectangle solved problem

---
+++ JS-Advanced/05.Classes and Attributes/01. Rectangle.js
@@ -0,0 +1,14 @@
+class Rectangle{
+    width;
+    height;
+    color;
+    constructor(width, height, color){
+        this.width = Number(width);
+        this.height = Number(height);
+        this.color = String(color)
+    }
+    
+    calcArea(){
+        return this.width * this.height
+    }
+}
