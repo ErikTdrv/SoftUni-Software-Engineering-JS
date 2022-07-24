9255fd33a00514a26a81877b42e1021848cbdb4e
ErikTdrv <eriktdrv@gmail.com>
2022-07-24 09:51:54 +0300

    Adding main app function for Furniture problem

---
+++ JS-Applications/07. JS-Applications-Routing/01.Furniture/src/app.js
@@ -0,0 +1,25 @@
+import page from "../node_modules/page/page.mjs"
+import { html, render } from 'https://unpkg.com/lit-html?module.js';
+import { loginView } from "../views/loginView.js";
+import {registerView } from "../views/registerView.js";
+import { loadDashboard } from "../views/dashboardView.js";
+
+window.addEventListener('load', loadDashboard)
+page('/login', loginView)
+page('/register', registerView)
+
+
+page.start()
+export function checkUser() {
+    const userInterface = document.getElementById('user');
+    const guestInterface = document.getElementById('guest');
+    if (localStorage.length == 0) {
+        userInterface.style.display = 'none';
+        guestInterface.style.display = 'inline';
+    }else {
+        userInterface.style.display = 'inline';
+        guestInterface.style.display = 'none';
+    }
+}
+checkUser()
+
