af836c0461096d900ae6de0e98a7078b139d1dcc
ErikTdrv <eriktdrv@gmail.com>
2022-07-26 00:52:04 +0300

    Adding CreateFurniture feature

--- JS-Applications/07. JS-Applications-Routing/01.Furniture/src/app.js
+++ JS-Applications/07. JS-Applications-Routing/01.Furniture/src/app.js
@@ -3,16 +3,21 @@ import { html, render } from 'https://unpkg.com/lit-html?module.js';
 import { loginView } from "../views/loginView.js";
 import {registerView } from "../views/registerView.js";
 import { loadDashboard } from "../views/dashboardView.js";
+import { furnitureView } from "../views/createFurnitureView.js";
+const logoutUrl = 'http://localhost:3030/users/logout';
 
 // window.addEventListener('load', loadDashboard)
 page('/catalog', loadDashboard)
 page('/login', loginView)
 page('/register', registerView)
+page('/logout', logout)
+page('/furniture', furnitureView)
 
 page.start()
 export function checkUser() {
     const userInterface = document.getElementById('user');
     const guestInterface = document.getElementById('guest');
+
     if (localStorage.length == 0) {
         userInterface.style.display = 'none';
         guestInterface.style.display = 'inline';
@@ -20,6 +25,19 @@ export function checkUser() {
         userInterface.style.display = 'inline';
         guestInterface.style.display = 'none';
     }
+}
+
+async function logout(){
+    fetch(logoutUrl, {
+        method: 'GET',
+        headers: {
+            'X-Authorization': localStorage.getItem('authToken')
+        }
+    });
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('ownerId')
+    checkUser()
+    page.redirect('/catalog')
 }
 checkUser()
 
