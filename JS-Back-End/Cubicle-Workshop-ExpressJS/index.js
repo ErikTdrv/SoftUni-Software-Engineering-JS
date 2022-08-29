f230b1adebaaf732a77395e0301ee330f9227b08
Erik Todorov <eriktdrv@gmail.com>
2022-08-29 19:37:38 +0300

    Adding routes

--- JS-Back-End/Cubicle-Workshop-ExpressJS/index.js
+++ JS-Back-End/Cubicle-Workshop-ExpressJS/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = 5000;
+const router = require('./config/routes')
 
 require('./config/handlebars')(app);
 app.use('/static', express.static('static'))
-app.get('/', (req, res) => {
-    res.render('index');
-});
+
+app.all('/', (req, res) => {
+    res.render('index')
+})
+app.use(router)
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
