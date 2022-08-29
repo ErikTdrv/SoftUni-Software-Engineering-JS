7cc09c13b166869dd6836cafcf281edf323b4adf
Erik Todorov <eriktdrv@gmail.com>
2022-08-29 20:14:37 +0300

    Adding functionality to create a cube

--- JS-Back-End/Cubicle-Workshop-ExpressJS/index.js
+++ JS-Back-End/Cubicle-Workshop-ExpressJS/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = 5000;
-const router = require('./config/routes')
+
+const cubeController = require('./config/routes')
+const homeController = require('./config/homeController')
 
 require('./config/handlebars')(app);
+app.use(express.urlencoded({extended: false}))
+
 app.use('/static', express.static('static'))
 
-app.all('/', (req, res) => {
-    res.render('index')
-})
-app.use(router)
+app.use(homeController)
+app.use('/cube', cubeController)
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
