7d78a21a31680e056a18f5ef51bded03b6b9cefd
Erik Todorov <eriktdrv@gmail.com>
2022-09-07 19:49:11 +0300

    Adding database(mongoose)

--- JS-Back-End/Cubicle-Workshop-ExpressJS/index.js
+++ JS-Back-End/Cubicle-Workshop-ExpressJS/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const initDatabase = require('./config/database');
 const app = express();
 const PORT = 5000;
 
 const cubeController = require('./config/routes')
-const homeController = require('./config/homeController')
+const homeController = require('./controllers/homeController')
 
 require('./config/handlebars')(app);
 app.use(express.urlencoded({extended: false}))
@@ -16,4 +17,11 @@ app.get('*', (req, res) => {
     res.render('404')
 })
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
+initDatabase()
+.then(() => {
+    app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
+})
+.catch(err => {
+    console.log(`Application init failed: ${err}`)
+})
