c3de7d45b47666d5f2ca9b9889b2b560f4a5d259
Erik Todorov <eriktdrv@gmail.com>
2022-09-22 11:25:39 +0300

    Adding get all accessories

--- JS-Back-End/Cubicle-Workshop-ExpressJS/services/accessoryService.js
+++ JS-Back-End/Cubicle-Workshop-ExpressJS/services/accessoryService.js
@@ -1,10 +1,13 @@
 const Accessory = require("../models/Accessory");
 
 const createAccessory = async (accessory) => {
-    console.log(accessory)
     return await Accessory.create(accessory)
+}
+const getAllAccessories = async () => {
+    return await Accessory.find({}).lean()
 }
 
 module.exports = {
     createAccessory,
+    getAllAccessories
 }
