f40b473088acfa32266e1485e5f4efdba8a68f34
Erik Todorov <eriktdrv@gmail.com>
2022-09-22 14:02:51 +0300

    Fixing Accessory model

--- JS-Back-End/Cubicle-Workshop-ExpressJS/models/Accessory.js
+++ JS-Back-End/Cubicle-Workshop-ExpressJS/models/Accessory.js
@@ -10,6 +10,17 @@ const accessorySchema = new mongoose.Schema({
         required: true,
         max: 100,
         min: 5, 
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function(v){
+                return /^https*/.test(v) 
+            }, 
+            message: props => `${props.value} it's not a valid URL`
+        },
+        // validate: [/^https*/, 'Invalid URL!'] // Might work
     }
 })
 
