1f661cf4c1fb88152f96350d7351749080da583e
Erik Todorov <eriktdrv@gmail.com>
2022-09-23 16:20:30 +0300

    Adding search functionality

--- JS-Back-End/Cubicle-Workshop-ExpressJS/services/cubeService.js
+++ JS-Back-End/Cubicle-Workshop-ExpressJS/services/cubeService.js
@@ -11,8 +11,22 @@ const getAllCubes = async () => {
 const getOneCube = async (_id) => {
     return await Cube.findById(_id).populate('accessories').lean()
 }
+const search = async (text, from, to) => {
+    let cubes = await getAllCubes();
+    if(text){
+        cubes = cubes.filter(x => x.name.toLowerCase() == text.toLowerCase())
+    }
+    if(from){
+        cubes = cubes.filter((cube) => cube.difficulty >= from);
+    }
+    if(to){
+        cubes = cubes.filter((cube) => cube.difficulty <= to)
+    }
+    return cubes;
+}
 
 module.exports = {
+    search,
     createCube,
     getAllCubes,
     getOneCube
