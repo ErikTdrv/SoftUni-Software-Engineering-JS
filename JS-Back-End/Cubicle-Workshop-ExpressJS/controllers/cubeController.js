8813dc24dae4fb7b7321b40100ea9c98e8ecc8c5
Erik Todorov <eriktdrv@gmail.com>
2022-09-25 21:30:44 +0300

    Creating cube controller

---
+++ JS-Back-End/Cubicle-Workshop-ExpressJS/controllers/cubeController.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router()
+const { createCube, getOneCube } = require("../services/cubeService");
+
+// Create route
+router.get('/create', (req, res) => {
+    res.render('create');
+});
+
+router.post('/create', async (req, res) => {
+    const cube = req.body;
+    await createCube(cube)
+    res.redirect('/');
+})
+//Details route
+router.get('/details/:id', async (req, res) => {
+    const id = req.params.id;
+    const cube = await getOneCube(id);
+    const accessories = cube.accessories;
+    res.render('details', { cube, accessories })
+})
+
+module.exports = router;
