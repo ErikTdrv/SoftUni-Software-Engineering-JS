b23a37f0d4ebd6cad0cef34f13ee725290851518
Erik Todorov <eriktdrv@gmail.com>
2022-09-25 21:30:52 +0300

    Refactoring....

--- JS-Back-End/Cubicle-Workshop-ExpressJS/controllers/homeController.js
+++ JS-Back-End/Cubicle-Workshop-ExpressJS/controllers/homeController.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const { getAllCubes } = require('../services/cubeService');
+const { getAllCubes, search } = require('../services/cubeService');
 
-async function homeController(req, res){
+router.get('/', async (req, res) => {
     const cubes = await getAllCubes()
     res.render('index', { cubes })
-}
-function aboutController(req, res){
+})
+router.get('/about', (req, res) => {
     res.render('about')
-}
+})
 
+router.get('/search', async (req, res) => {
+    const body = req.query;
+    const cubes = await search(body.search, body.from, body.to);
+    res.render('index', {
+        cubes,
+        title: 'Search',
+        'search': body.search, 'from': body.from, 'to': body.to
+    })
+})
 
-module.exports = {
-    homeController, aboutController
-}
+module.exports = router;
