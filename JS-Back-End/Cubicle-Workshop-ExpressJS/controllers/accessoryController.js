47465de2e89f21fad9d179d544c139b24575792a
Erik Todorov <eriktdrv@gmail.com>
2022-09-25 21:30:30 +0300

    Creating accessory controller

---
+++ JS-Back-End/Cubicle-Workshop-ExpressJS/controllers/accessoryController.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { createAccessory, getAllWithout, attachAccessory } = require('../services/accessoryService');
+const { getOneCube } = require('../services/cubeService');
+const router = express.Router()
+
+router.get('/create', (req, res) => {
+    res.render('accessory/create')
+})
+router.post('/create', async (req, res) => {
+    const accessory = req.body;
+    await createAccessory(accessory);
+    res.redirect('/')
+})
+router.get('/attach/:id', async (req, res) => {
+    const cubeId = req.params.id;
+    const cube = await getOneCube(cubeId)
+    const accessoriesArr = cube.accessories?.map((e) => e._id);
+    const accessories = await getAllWithout(accessoriesArr);
+    res.render('accessory/attach', { cube, accessories });
+})
+
+router.post('/attach/:id', async (req, res) => {
+    const cubeId = req.params.id;
+    const accessoryId = req.body.accessory
+    await attachAccessory(cubeId, accessoryId)
+    res.redirect('/')
+})
+
+
+module.exports = router;
