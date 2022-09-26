6cb91c89ffa89d35f67001fda8d9584d9b395dcb
Erik Todorov <eriktdrv@gmail.com>
2022-09-26 19:30:15 +0300

    Updating routes

--- JS-Back-End/Cubicle-Workshop-ExpressJS/controllers/authController.js
+++ JS-Back-End/Cubicle-Workshop-ExpressJS/controllers/authController.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const User = require('../models/User');
+const { registerUser, checkForUser } = require('../services/authService');
 const router = express.Router()
 
+//Register routes
 router.get('/register', (req, res) => {
     res.render('auth/registerPage')
 })
+router.post('/register', async (req, res) => {
+    try{
+        const {username, password, rePass} = req.body;
+        await registerUser(username, password)
+    }catch(err){
+        return res.status(400).send(err)
+    }
+    res.redirect('/login');
+})
 
+//Login routes
 router.get('/login', (req, res) => {
     res.render('auth/loginPage')
+})
+router.post('/login', async (req, res) => {
+        const { username, password } = req.body;
+        const isValid = await checkForUser(username, password);
+        if(isValid){
+            res.redirect('/')
+        }else {
+            res.status(400).send('Invalid username or password')
+        }
 })
 
 
