512bf67857478bc205d2bbc9a0c301fa7181df0d
Erik Todorov <eriktdrv@gmail.com>
2022-09-30 18:30:05 +0300

    Making validations for User model

--- JS-Back-End/Cubicle-Workshop-ExpressJS/models/User.js
+++ JS-Back-End/Cubicle-Workshop-ExpressJS/models/User.js
@@ -3,11 +3,16 @@ const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
     'username': {
         type: String,
+        unique: true,
         required: true,
+        validate: [/^[A-Za-z0-9]+$/, 'Username should consist only of English letters and digits!'],
+        minlength: [5, 'Should consist minimum 5 characters!']
     },
     'password': {
         type: String,
+        validate: [/^[A-Za-z0-9]+$/, 'Password should consist only of English letters and digits!'],
         required: true,
+        minlength: [5, 'Should consist minimum 8 characters!']
     }
 })
 
