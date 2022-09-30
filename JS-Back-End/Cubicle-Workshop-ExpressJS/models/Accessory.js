abd9847442742aa5f8d5ea3ce3b69486e52f97a5
Erik Todorov <eriktdrv@gmail.com>
2022-09-30 18:29:48 +0300

    Making validations for Accessory model

--- JS-Back-End/Cubicle-Workshop-ExpressJS/models/Accessory.js
+++ JS-Back-End/Cubicle-Workshop-ExpressJS/models/Accessory.js
@@ -2,22 +2,24 @@ const mongoose = require('mongoose');
 
 const accessorySchema = new mongoose.Schema({
     name: {
-        type: String, 
+        type: String,
         required: true,
+        minlength: [5, 'Should consist at least 5 characters!'],
+        validate: [/^[A-Za-z0-9\s]+$/, 'Name should consist only of English letters, digits and whitespaces!'],
     },
     description: {
         type: String,
         required: true,
-        max: 100,
-        min: 5, 
+        minlength: 20,
+        validate: [/^[A-Za-z0-9\s]+$/, 'Description should consist only of English letters, digits and whitespaces!'],
     },
     imageUrl: {
         type: String,
         required: true,
         validate: {
-            validator: function(v){
-                return /^https*/.test(v) 
-            }, 
+            validator: function (v) {
+                return /^https*/.test(v)
+            },
             message: props => `${props.value} it's not a valid URL`
         },
         // validate: [/^https*/, 'Invalid URL!'] // Might work
