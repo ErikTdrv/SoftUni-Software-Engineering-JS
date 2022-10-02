8d170ee475b38b0f34f4762b6a23373b640226ad
Erik Todorov <eriktdrv@gmail.com>
2022-10-02 09:25:41 +0300

    Fixing paths...

--- JS-Back-End/Cubicle-Workshop-ExpressJS/controllers/accessoryController.js
+++ JS-Back-End/Cubicle-Workshop-ExpressJS/controllers/accessoryController.js
@@ -14,7 +14,7 @@ router.post('/create', async (req, res) => {
 })
 
 //Attach accessory
-router.get('/attach/:id', async (req, res) => {
+router.get('/:id/attach', async (req, res) => {
     const cubeId = req.params.id;
     const cube = await getOneCube(cubeId)
     const accessoriesArr = cube.accessories?.map((e) => e._id);
@@ -22,7 +22,7 @@ router.get('/attach/:id', async (req, res) => {
     res.render('accessory/attach', { cube, accessories });
 })
 
-router.post('/attach/:id', async (req, res) => {
+router.post('/:id/attach', async (req, res) => {
     const cubeId = req.params.id;
     const accessoryId = req.body.accessory
     await attachAccessory(cubeId, accessoryId)
