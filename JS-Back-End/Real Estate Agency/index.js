6bb0d6792caee5c473667e7a1dff1f8ebadffcbb
Erik Todorov <eriktdrv@gmail.com>
2022-10-03 22:29:40 +0300

    Starting the server

--- JS-Back-End/Real Estate Agency/index.js
+++ JS-Back-End/Real Estate Agency/index.js
@@ -1,8 +1,25 @@
+const cookieParser = require('cookie-parser');
 const express = require('express');
+const initDatabase = require('./config/database');
+const authMiddleware = require('./middlewares/authMiddleware');
+const router = require('./routes');
 const app = express();
+const PORT = 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
-    res.end()
+require('./config/handlebars')(app);
+app.use(express.urlencoded({extended: true}))
+
+app.use('/static', express.static('static'))
+app.use(cookieParser())
+app.use(authMiddleware)
+app.use(router)
+
+
+//Set database and run server
+initDatabase()
+.then(() => {
+    app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
 })
-app.listen(3000, () => console.log('Server listening on port 3000'))
+.catch(err => {
+    console.log(`Application init failed: ${err}`)
+})
