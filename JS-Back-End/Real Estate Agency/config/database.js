8b31d2591aebde036a331c62d12f098226228ba5
Erik Todorov <eriktdrv@gmail.com>
2022-10-03 22:29:55 +0300

    Configuration of the database

---
+++ JS-Back-End/Real Estate Agency/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+function initDatabase() {
+    // return mongoose.connect('mongodb://0.0.0.0:27017/cubes');
+    return mongoose.connect('mongodb://localhost:27017/cubes');
+} 
+
+
+module.exports = initDatabase;
