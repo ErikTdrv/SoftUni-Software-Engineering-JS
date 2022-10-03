1bd59fba74356653aef12164940422b0526d6c6b
Erik Todorov <eriktdrv@gmail.com>
2022-10-03 23:22:43 +0300

    fixing database

--- JS-Back-End/Real Estate Agency/config/database.js
+++ JS-Back-End/Real Estate Agency/config/database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 function initDatabase() {
-    // return mongoose.connect('mongodb://0.0.0.0:27017/cubes');
-    return mongoose.connect('mongodb://localhost:27017/cubes');
+    return mongoose.connect('mongodb://0.0.0.0:27017/cubes');
+    // return mongoose.connect('mongodb://localhost:27017/cubes');
 } 
 
 
