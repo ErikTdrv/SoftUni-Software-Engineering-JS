3cae04baa4677da1564bf641bad20b92bba756ac
Erik Todorov <eriktdrv@gmail.com>
2022-10-05 00:26:43 +0300

    Adding authController

--- JS-Back-End/Real Estate Agency/routes.js
+++ JS-Back-End/Real Estate Agency/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router()
+const authController = require('./controllers/authController')
 
 router.get('/', (req, res) => {
     res.render('home')
 })
+router.use(authController)
 
 module.exports = router;
