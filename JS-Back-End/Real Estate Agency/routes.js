e634e3c5db28fcc1435cd39b9d7196cd72d76b1f
Erik Todorov <eriktdrv@gmail.com>
2022-10-05 23:09:18 +0300

    Creating create func

--- JS-Back-End/Real Estate Agency/routes.js
+++ JS-Back-End/Real Estate Agency/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router()
 const authController = require('./controllers/authController')
+const homeController = require('./controllers/homeController')
 
 router.get('/', (req, res) => {
     res.render('home')
 })
 router.use(authController)
+router.use(homeController)
 
 module.exports = router;
