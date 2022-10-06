254e0397bd6c3430c654f0eaaa7715e431d076d1
Erik Todorov <eriktdrv@gmail.com>
2022-10-06 16:46:42 +0300

    Adding home page

--- JS-Back-End/Real Estate Agency/routes.js
+++ JS-Back-End/Real Estate Agency/routes.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router()
 const authController = require('./controllers/authController')
-const homeController = require('./controllers/homeController')
+const homeController = require('./controllers/homeController');
 
-router.get('/', (req, res) => {
-    res.render('home')
-})
+
 router.use(authController)
 router.use(homeController)
 
