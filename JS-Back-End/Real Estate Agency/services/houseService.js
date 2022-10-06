d21b2a30ae1b07e878fe5c07984afce0d76bec6f
Erik Todorov <eriktdrv@gmail.com>
2022-10-06 16:46:36 +0300

    Adding home page

--- JS-Back-End/Real Estate Agency/services/houseService.js
+++ JS-Back-End/Real Estate Agency/services/houseService.js
@@ -1,8 +1,10 @@
 const House = require("../models/House")
 const User = require("../models/User")
 
-const getLast3Houses = () => {
-    User.find({}).sort()
+const getLast3Houses = async () => {
+    const houses = await House.find({}).lean();
+    const last3Houses = houses.slice(-3)
+    return last3Houses
 }
 const createHouse = async (name, type, year, city, imageUrl, description, pieces, owner) => {
     await House.create({name, type, year, city, imageUrl, description, pieces, owner})
