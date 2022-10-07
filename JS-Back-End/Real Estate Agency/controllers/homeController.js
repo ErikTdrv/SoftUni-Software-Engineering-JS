7458163530a22d21dfa61dab6c8d39b737082e84
Erik Todorov <eriktdrv@gmail.com>
2022-10-07 15:05:36 +0300

    Adding edit & delete & search functionalities

--- JS-Back-End/Real Estate Agency/controllers/homeController.js
+++ JS-Back-End/Real Estate Agency/controllers/homeController.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createHouse, getLast3Houses, getOneHouse } = require('../services/houseService');
+const { createHouse, getLast3Houses, getOneHouse, getAllHouses, deleteHouse } = require('../services/houseService');
 const router = express.Router();
 
 //Home
@@ -28,18 +28,46 @@ router.get('/details/:id', async (req, res) => {
     let isOwn = false;
     let alreadyRented = false;
     let noMoreHousing = false;
-    const userId = req.user._id;
+    const userId = req.user?._id;
     const id = req.params.id;
     const house = await getOneHouse(id)
-    if(house.pieces == 0){
+    if(house?.pieces == 0){
         noMoreHousing = true;
     }
-    if(house.rentedBy.includes(userId)){
+    if(house?.rentedBy.includes(userId)){
         alreadyRented = true;
     }
-    if(house.owner == userId){
+    if(house?.owner == userId){
         isOwn = true;
     }
     res.render('house/details', { house, isOwn, alreadyRented, noMoreHousing })
+})
+
+//Housing for rent
+router.get('/rent', async (req, res) => {
+    const apartments = await getAllHouses();
+    res.render('house/aprt-for-recent', { apartments })
+})
+
+//Edit
+router.get('/details/:id/edit', async ( req, res) => {
+    const id = req.params.id;
+    const house = await getOneHouse(id);
+    res.render('house/edit', house )
+})
+router.get('/details/:id/delete', async (req, res) => {
+    const id = req.params.id;
+    await deleteHouse(id)
+    res.redirect('/')
+})
+
+//Search
+router.get('/search', async (req, res) => {
+    res.render('search')
+})
+router.post('/search', async (req, res) => {
+    const body = req.body.name;
+    const apartments = await getOneHouse(null, body)
+    res.render('search', {apartments})
 })
 module.exports = router;
