3312fd67f54723ab2f90e46544d1eb114e00cfce
Erik Todorov <eriktdrv@gmail.com>
2022-10-07 15:05:40 +0300

    Adding edit & delete & search functionalities

--- JS-Back-End/Real Estate Agency/services/houseService.js
+++ JS-Back-End/Real Estate Agency/services/houseService.js
@@ -9,12 +9,25 @@ const getLast3Houses = async () => {
 const createHouse = async (name, type, year, city, imageUrl, description, pieces, owner) => {
     await House.create({name, type, year, city, imageUrl, description, pieces, owner})
 }
-const getOneHouse = async (_id) => {
-    const house = await House.findById(_id).lean()
-    return house
+const getOneHouse = async (_id, name) => {
+    if(_id){
+        const house = await House.findById(_id).lean()
+        return house
+    }else if(name){
+        let apartments = await House.find({}).lean()
+        apartments = apartments.filter(x => x.name.toLowerCase().includes(name.toLowerCase()))
+        return apartments
+    }
 }
-
+const getAllHouses = async () => {
+    return await House.find({}).lean();
+}
+const deleteHouse = async (id) => {
+    await House.findByIdAndDelete(id)
+}
 module.exports = {
+    deleteHouse,
+    getAllHouses,
     getOneHouse,
     getLast3Houses, 
     createHouse
