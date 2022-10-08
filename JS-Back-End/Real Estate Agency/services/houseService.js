c7791b1ea5347bcb9c6c140f3c8f55a4bde4ec9b
Erik Todorov <eriktdrv@gmail.com>
2022-10-08 18:31:55 +0300

    Rent home functionality

--- JS-Back-End/Real Estate Agency/services/houseService.js
+++ JS-Back-End/Real Estate Agency/services/houseService.js
@@ -1,3 +1,5 @@
+const { findById } = require("../models/House");
+const { getUser } = require('../services/authService')
 const House = require("../models/House")
 const User = require("../models/User")
 
@@ -31,7 +33,18 @@ const editHouse = async (id, body) => {
         runValidators: true,
     })
 }
+const rentHome = async (id, user) => {
+    const rentingUser = {
+        name: user.name,
+        _id: user._id
+    }
+    const house = await House.findById(id);
+    house.rentedBy.push(rentingUser)
+    house.save()
+}
+
 module.exports = {
+    rentHome,
     editHouse,
     deleteHouse,
     getAllHouses,
