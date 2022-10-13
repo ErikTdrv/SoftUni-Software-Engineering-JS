860a6ee54306efa01c15bb7bb110a69e4420601a
Erik Todorov <eriktdrv@gmail.com>
2022-10-13 23:19:40 +0300

    Create index.js

---
+++ JS-Back-End/BookingUni/index.js
@@ -0,0 +1,23 @@
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const initDatabase = require('./config/database');
+const router = require('./routes');
+const app = express();
+const PORT = 5000;
+
+require('./config/handlebars')(app);
+app.use(express.urlencoded({extended: true}))
+
+app.use('/static', express.static('static'))
+app.use(cookieParser())
+app.use(router)
+
+
+//Set database and run server
+initDatabase()
+.then(() => {
+    app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
+})
+.catch(err => {
+    console.log(`Application init failed: ${err}`)
+})
