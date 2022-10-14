09a091cedfa3b12b4ab062bbc651cb8ec064ade1
Erik Todorov <eriktdrv@gmail.com>
2022-10-14 15:30:18 +0300

    Adding home page

--- JS-Back-End/BookingUni/controllers/homeController.js
+++ JS-Back-End/BookingUni/controllers/homeController.js
@@ -1,6 +1,10 @@
+const { getHotelsSort } = require('../services/hotelService');
+
 const router = require('express').Router();
 
-    router.get('/', (req, res) => {
-        res.render('home')
+    router.get('/', async (req, res) => {
+        const hotels = await getHotelsSort()
+        console.log(hotels)
+        res.render('hotel/home', { hotels })
     })
 module.exports = router;
