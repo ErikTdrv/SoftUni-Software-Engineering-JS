20cf37a61de30341504158b9f7f5859d863b0692
Erik Todorov <eriktdrv@gmail.com>
2022-10-15 09:17:57 +0300

    Adding profile page

--- JS-Back-End/BookingUni/controllers/homeController.js
+++ JS-Back-End/BookingUni/controllers/homeController.js
@@ -1,9 +1,17 @@
-const { getHotelsSort } = require('../services/hotelService');
+const { isUser } = require('../middlewares/authMiddleware');
+const { getHotelsSort, getBookedHotels, getOneUser } = require('../services/hotelService');
 
 const router = require('express').Router();
 
     router.get('/', async (req, res) => {
         const hotels = await getHotelsSort()
         res.render('hotel/home', { hotels })
+    })
+    router.get('/profile', isUser ,async (req, res) => {
+        const userId = req.user._id;
+        const user = await getOneUser(userId);
+        let reservations = await getBookedHotels(userId)
+        reservations = reservations.join(', ')
+        res.render('hotel/profile', { user , reservations})
     })
 module.exports = router;
