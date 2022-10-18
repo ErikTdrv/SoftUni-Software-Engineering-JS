d70f751640b897ec76e91ff117e9a29278d75ab4
Erik Todorov <eriktdrv@gmail.com>
2022-10-18 16:34:56 +0300

    Adding create page

--- JS-Back-End/Book-Talk/controllers/homeController.js
+++ JS-Back-End/Book-Talk/controllers/homeController.js
@@ -1,9 +1,27 @@
+const { createBook } = require('../services/bookService');
+
 const router = require('express').Router();
 
 //Get home page
 router.get('/', (req, res) => {
     res.render('home')
 })
+
+//Create page
+router.get('/create', (req, res) => {
+    res.render('book/create')
+})
+router.post('/create', async (req, res) => {
+    const {title,author, genre, stars, review, imageUrl} = req.body;
+    const userId = req?.user._id
+    try {
+        await createBook(title,author, genre, stars, review, imageUrl, userId)
+        res.redirect('/')
+    } catch (error) {
+        res.render('book/create', {error: error.message})
+    }
+})
+
 
 
 module.exports = router;
