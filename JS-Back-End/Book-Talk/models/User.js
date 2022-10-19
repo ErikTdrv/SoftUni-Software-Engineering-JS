9d65d1b41874bd5dc18abe87a038bbee1c6e5a98
Erik Todorov <eriktdrv@gmail.com>
2022-10-19 17:16:29 +0300

    Adding more validation to User model

--- JS-Back-End/Book-Talk/models/User.js
+++ JS-Back-End/Book-Talk/models/User.js
@@ -5,14 +5,17 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        minLength: [4, 'Username must have more than 4 characters!']
     },
     email: {
         type: String,
         required: true,
+        minLength: [10, 'Email must have more than 10 characters!']
     }, 
     password: {
         type: String,
         required: true,
+        minLength: [3, 'Password must be longer than 3 characters!']
     }
 })
 
