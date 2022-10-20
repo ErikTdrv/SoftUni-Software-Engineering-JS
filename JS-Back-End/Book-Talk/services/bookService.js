d337c2c8c1eff52d0cc614f86479d09c62568e09
Erik Todorov <eriktdrv@gmail.com>
2022-10-20 15:45:52 +0300

    Adding getProfileBooks service

--- JS-Back-End/Book-Talk/services/bookService.js
+++ JS-Back-End/Book-Talk/services/bookService.js
@@ -24,7 +24,12 @@ const updateBook = async (bookId, title,author, genre, stars, review, imageUrl) 
 const deleteBook = async (bookId) => {
     await Book.findByIdAndRemove(bookId)
 }
+const getProfileBooks = async (userId) => {
+    const books = await Book.find({owner: userId}).lean()
+    return books
+}
 module.exports = {
+    getProfileBooks,
     deleteBook,
     updateBook,
     wishOneBook,
