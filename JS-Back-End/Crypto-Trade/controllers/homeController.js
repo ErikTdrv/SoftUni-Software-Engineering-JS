e363007bd57f06aed21aa6f26b80dca75f73c6c2
Erik Todorov <eriktdrv@gmail.com>
2022-10-26 16:49:59 +0300

    Adding create page

--- JS-Back-End/Crypto-Trade/controllers/homeController.js
+++ JS-Back-End/Crypto-Trade/controllers/homeController.js
@@ -1,4 +1,4 @@
-const { getAll } = require('../services/cryptoService');
+const { getAll, createCrypto } = require('../services/cryptoService');
 
 const router = require('express').Router();
 
@@ -8,5 +8,19 @@ router.get('/', (req, res) => {
 router.get('/all', async (req, res) => {
     const cryptos = await getAll()
     res.render('catalog', { crypto: cryptos})
+})
+
+//Create
+router.get('/create', (req, res) => {
+    res.render('create')
+})
+router.post('/create', async (req, res) => {
+    const crypto = req.body;
+    try {
+        await createCrypto(crypto)
+        res.redirect('/')
+    } catch (error) {
+        res.render('create', { error: error.message})
+    }
 })
 module.exports = router;
