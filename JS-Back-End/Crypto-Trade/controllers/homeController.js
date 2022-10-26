802d10d390d92c129ac4847b52f704d698eb7032
Erik Todorov <eriktdrv@gmail.com>
2022-10-26 16:39:43 +0300

    Adding catalog functionality

--- JS-Back-End/Crypto-Trade/controllers/homeController.js
+++ JS-Back-End/Crypto-Trade/controllers/homeController.js
@@ -1,6 +1,12 @@
+const { getAll } = require('../services/cryptoService');
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.render('home')
+})
+router.get('/all', async (req, res) => {
+    const cryptos = await getAll()
+    res.render('catalog', { crypto: cryptos})
 })
 module.exports = router;
---
+++ JS-Back-End/Crypto-Trade/services/cryptoService.js
@@ -0,0 +1,12 @@
+const Crypto = require('../models/Crypto');
+const getAll = async () => {
+    return await Crypto.find({}).lean()
+}
+const createCrypto = async (crypto) => {
+    await Crypto.create({crypto})
+}
+
+module.exports = {
+    getAll,
+    createCrypto
+}
