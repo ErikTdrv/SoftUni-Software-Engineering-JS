4bddd6e40b8e8d4c5af3047cca167c9340a1d2ad
Erik Todorov <eriktdrv@gmail.com>
2022-10-27 15:50:25 +0300

    Adding auth middleware

--- JS-Back-End/Crypto-Trade/index.js
+++ JS-Back-End/Crypto-Trade/index.js
@@ -3,6 +3,7 @@ const app = express();
 const router = require('./routes');
 const cookieParser = require('cookie-parser');
 const initDatabase = require('./configs/initDatabase');
+const { authMiddleware } = require('./middlewares/authMiddleware');
 
 
 require('./configs/initHandlebars')(app);
@@ -10,9 +11,9 @@ app.use(express.urlencoded({extended: true}))
 
 app.use('/static', express.static('static'))
 app.use(cookieParser())
+app.use(authMiddleware)
 // app.use(authMiddleware)
 app.use(router)
-
 
 //Set database and run server
 initDatabase()
